46af3ff46243be659421dd3a14434a0c1c8f246f
alex-abanin <aa@occam.fi>
2024-11-23 16:39:01 +0300

    use app-kit

--- src/pages/ui/layout/header.tsx
+++ src/pages/ui/layout/header.tsx
@@ -1,6 +1,6 @@
 import {
-  // AppKitConnectWalletButton,
-  ConnectWalletButton,
+  AppKitConnectWalletButton,
+  // ConnectWalletButton,
 } from "features/connect-wallet";
 
 export const Header = () => {
@@ -8,9 +8,9 @@ export const Header = () => {
     <nav className="mx-10 mt-6 flex items-center">
       <img className="w-[113px]" src="/logo.png" alt="log" />
 
-      {/* <AppKitConnectWalletButton className="ml-auto" /> */}
+      <AppKitConnectWalletButton className="ml-auto" />
 
-      <ConnectWalletButton className="ml-auto" />
+      {/* <ConnectWalletButton className="ml-auto" /> */}
     </nav>
   );
 };
